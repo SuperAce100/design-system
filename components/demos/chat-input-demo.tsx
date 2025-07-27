90187a4f4edbe2c1c318e009953ef5183c44be3f
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2025-07-27 13:00:17 -0700

    Chat input

--- components/demos/chat-input-demo.tsx
+++ components/demos/chat-input-demo.tsx
@@ -1,5 +1,42 @@
+"use client";
+
 import ChatInput from "@/registry/new-york/blocks/chat-input/chat-input";
+import { useState } from "react";
+
+const models = [
+  { id: "gpt-4o", name: "GPT-4o", description: "GPT-4o" },
+  { id: "claude-3-7-sonnet", name: "Claude 3.7 Sonnet", description: "Claude 3.7 Sonnet" },
+  {
+    id: "gemini-2.5-pro",
+    name: "Gemini 2.5 Pro",
+    description: "Google's most powerful reasoning model",
+  },
+];
 
 export default function ChatInputDemo() {
-  return <ChatInput />;
+  const [input, setInput] = useState("");
+  const [model, setModel] = useState("gpt-4o");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+  return (
+    <ChatInput
+      title="Chat Input"
+      placeholder="Type something..."
+      models={models}
+      allowFileUpload={false}
+      loading={loading}
+      handleSubmit={handleSubmit}
+      handleInputChange={(e) => setInput(e.target.value)}
+      input={input}
+      model={model}
+      onModelChange={(value) => setModel(value)}
+    />
+  );
 }
--- registry/new-york/blocks/chat-input/chat-input.tsx
+++ registry/new-york/blocks/chat-input/chat-input.tsx
@@ -1,16 +1,90 @@
-import { Button } from "@/registry/new-york/blocks/button/button";
-import { Textarea } from "@/registry/new-york/blocks/textarea/textarea";
-import { Send } from "lucide-react";
+"use client";
 
-export default function ChatInput() {
+import { Button } from "@/registry/new-york/blocks/button/button";
+import { SendIcon, UploadIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/registry/new-york/blocks/select/select";
+import { Textarea } from "@/registry/new-york/blocks/textarea/textarea";
+
+export default function ChatInput({
+  title,
+  placeholder,
+  models,
+  allowFileUpload,
+  loading,
+  handleSubmit,
+  handleInputChange,
+  input,
+  className,
+  model,
+  onModelChange,
+}: {
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  handleInputChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  input: string;
+  className?: string;
+  model: string;
+  onModelChange: (model: string) => void;
+  title: string;
+  placeholder: string;
+  models: { id: string; name: string; description: string }[];
+  allowFileUpload: boolean;
+  loading: boolean;
+}) {
   return (
-    <div className="flex flex-row items-center justify-between gap-2 max-w-xl relative w-full group">
-      <Textarea placeholder="Type something..." showHandle={false} className="w-full min-h-24" />
-      <div className="flex flex-row h-10 items-end justify-between gap-2 absolute left-2 right-2 bottom-2">
-        <Button variant="default" size="icon" className="ml-auto">
-          <Send />
-        </Button>
+    <form
+      onSubmit={handleSubmit}
+      className={cn("w-full relative flex flex-col gap-4 items-center", className)}
+    >
+      {title && <h2 className="text-5xl font-medium tracking-tight">{title}</h2>}
+      <Textarea
+        name="prompt"
+        value={input}
+        onChange={handleInputChange}
+        disabled={loading}
+        autoComplete="off"
+        placeholder={placeholder}
+        className="w-full pb-20 pt-4 px-4 rounded-xl text-md bg-background disabled:opacity-100 disabled:cursor-not-allowed disabled:bg-background"
+      />
+      <Button
+        type="submit"
+        disabled={loading}
+        variant="default"
+        size="icon"
+        className="absolute bottom-2 right-2"
+      >
+        <SendIcon className="w-4 h-4" />
+      </Button>
+      <div className="flex items-center gap-2 absolute bottom-0 left-0">
+        <Select value={model} onValueChange={(value) => onModelChange(value)}>
+          <SelectTrigger className="w-[200px] shadow-none pl-4 focus-visible:ring-0 border-none bg-transparent text-xs text-muted-foreground">
+            <SelectValue>
+              {models.find((m) => m.id === model)?.name || "Select a model"}
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent className="rounded-xl shadow-gray-200">
+            {models.map((model) => (
+              <SelectItem key={model.id} value={model.id} className="rounded-lg focus:bg-primary/5">
+                <div className="flex flex-col">
+                  <span>{model.name}</span>
+                  <span className="text-xs text-muted-foreground">{model.description}</span>
+                </div>
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        {allowFileUpload && (
+          <Button variant="outline" size="icon">
+            <UploadIcon className="w-4 h-4" />
+          </Button>
+        )}
       </div>
-    </div>
+    </form>
   );
 }
