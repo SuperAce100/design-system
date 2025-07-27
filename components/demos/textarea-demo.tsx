cc1074aaf05ad5c853cdf9712599335c607d2f30
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2025-07-27 16:04:25 -0700

    Small changes

--- components/demos/textarea-demo.tsx
+++ components/demos/textarea-demo.tsx
@@ -1,5 +1,5 @@
 import { Textarea } from "@/registry/new-york/blocks/textarea/textarea";
 
 export default function TextareaDemo() {
-  return <Textarea placeholder="Type something..." />;
+  return <Textarea placeholder="Type something..." className="max-w-sm min-h-32" />;
 }
