8ef16248a7ed8544956ffa6a15d48fc9223611f0
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2025-08-18 23:55:33 -0700

    button improvements

--- registry/new-york/blocks/button/button.tsx
+++ registry/new-york/blocks/button/button.tsx
@@ -5,23 +5,23 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-xl text-md font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive duration-300",
+  "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-xl text-md font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive duration-200 hover:duration-75 transition-all",
   {
     variants: {
       variant: {
         default:
-          "border border-primary bg-primary/30 bg-gradient-to-b from-primary to-primary/50 text-primary-foreground shadow-lg shadow-slate-200/50 transition-all duration-200 hover:bg-white hover:shadow-lg active:bg-primary active:shadow-none hover:-translate-y-px active:translate-y-px",
+          "border border-primary bg-primary/30 bg-gradient-to-b from-primary to-primary/70 text-primary-foreground shadow-lg dark:shadow-none shadow-slate-200/50 hover:bg-white dark:hover:bg-primary hover:shadow-lg active:bg-primary active:shadow-none hover:-translate-y-px active:translate-y-px ",
         fancy:
-          "border border-primary bg-primary/30 bg-radial-[at_50%_25%] from-primary/40 to-primary text-primary-foreground shadow-md inset-shadow-sm inset-shadow-white/50 transition-all duration-200 hover:bg-white hover:shadow-lg hover:inset-shadow-white active:bg-primary/30 active:shadow-none active:inset-shadow-primary/80  hover:-translate-y-px active:translate-y-px",
+          "border border-primary bg-primary/30 bg-radial-[at_50%_25%] from-primary/40 to-primary text-primary-foreground shadow-md inset-shadow-sm inset-shadow-white/50 hover:bg-white hover:shadow-lg hover:inset-shadow-white active:bg-primary/30 active:shadow-none active:inset-shadow-primary/80  hover:-translate-y-px active:translate-y-px",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border shadow-lg hover:shadow-none shadow-slate-200/50 bg-transparent text-primary transition-all duration-200 hover:border-transparent hover:bg-primary/10 active:bg-primary/30",
+          "border shadow-lg dark:shadow-none shadow-slate-200/50 bg-transparent text-primary hover:border-transparent hover:bg-primary/10 active:bg-primary/30",
         secondary:
-          "border border-transparent bg-primary/10 text-primary transition-all duration-200 hover:border-primary/50 hover:bg-transparent  active:border-transparent active:border-primary active:ring-1",
+          "border border-transparent bg-primary/10 text-primary hover:border-primary/50 hover:bg-transparent  active:border-transparent active:border-primary active:ring-1",
         ghost:
-          "bg-transparent text-primary transition-all duration-200 hover:border-transparent hover:bg-primary/10 active:bg-primary/30",
-        link: "text-primary underline-offset-8 hover:underline-offset-4 active:underline-offset-2 hover:opacity-70 hover:underline active:text-foreground duration-100",
+          "bg-transparent text-primary hover:border-transparent hover:bg-primary/10 active:bg-primary/30",
+        link: "text-foreground underline decoration-primary underline-offset-2 hover:underline-offset-2 active:underline-offset-4 hover:opacity-70 hover:decoration-2 active:text-foreground",
       },
       size: {
         default: "px-4 py-2",
