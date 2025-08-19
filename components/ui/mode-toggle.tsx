5b018c00d2daca54fe9a1a8497057f373c0ce547
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2025-08-19 00:30:16 -0700

    Animate theme toggler

--- components/ui/mode-toggle.tsx
+++ components/ui/mode-toggle.tsx
@@ -1,33 +1,11 @@
 "use client";
 
-import { MoonIcon, SunIcon } from "lucide-react";
-
-import { Toggle } from "@/components/ui/toggle";
-import { useTheme } from "next-themes";
+import { AnimatedThemeToggler } from "@/components/magicui/animated-theme-toggler";
 
 export default function ModeToggle({ className }: { className?: string }) {
-  const { theme, setTheme } = useTheme();
-
   return (
     <div className={className}>
-      <Toggle
-        variant="outline"
-        className="group data-[state=on]:hover:bg-muted size-9 data-[state=on]:bg-transparent"
-        pressed={theme === "dark"}
-        onPressedChange={() => setTheme((prev: string) => (prev === "dark" ? "light" : "dark"))}
-        aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
-      >
-        <MoonIcon
-          size={16}
-          className="shrink-0 scale-0 opacity-0 transition-all duration-300 dark:scale-100 dark:opacity-100"
-          aria-hidden="true"
-        />
-        <SunIcon
-          size={16}
-          className="absolute shrink-0 scale-100 opacity-100 transition-all duration-300 dark:scale-0 dark:opacity-0"
-          aria-hidden="true"
-        />
-      </Toggle>
+      <AnimatedThemeToggler />
     </div>
   );
 }
