2bbaa996aac21e8e6d0fd4c1b4aed8526ee9c2a1
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2025-08-20 16:14:51 -0700

    Switch to UIMessage

--- components/demos/chat-message-demo.tsx
+++ components/demos/chat-message-demo.tsx
@@ -1,22 +1,68 @@
-import ChatMessage, { Message } from "@/registry/new-york/blocks/chat-message/chat-message";
+import ChatMessage from "@/registry/new-york/blocks/chat-message/chat-message";
+import { UIMessage } from "ai";
 
-const messages: Message[] = [
+const messages: UIMessage[] = [
   {
+    id: "1",
     role: "user",
-    content: "Hello, how are you?",
+    parts: [
+      {
+        type: "text",
+        text: "Hello, how are you?",
+      },
+    ],
   },
   {
+    id: "2",
     role: "assistant",
-    content: "I'm good, thank you!",
+    parts: [
+      {
+        type: "text",
+        text: "I'm good, thank you!",
+      },
+    ],
   },
   {
+    id: "3",
     role: "user",
-    content: "Tell me about design systems",
+    parts: [
+      {
+        type: "text",
+        text: "Tell me about design systems",
+      },
+    ],
   },
   {
+    id: "4",
     role: "assistant",
-    content:
-      "# Design Systems\n\nA design system is a **collection of components** that are used to build a design system. It is a collection of components that are used to build a design system. ",
+    parts: [
+      {
+        type: "reasoning",
+        text: "I'm thinking about design systems and I'm going to use the web_search tool to help me.",
+      },
+      {
+        type: "dynamic-tool",
+        toolName: "web_search",
+        toolCallId: "1",
+        input: {
+          query: "what is a design system?",
+        },
+        output: {
+          results: [
+            "A design system is a **collection of components** that are used to build a design system. It is a collection of components that are used to build a design system. ",
+          ],
+        },
+        state: "output-available",
+      },
+      {
+        type: "reasoning",
+        text: "Now I'm going to return the results of the web_search tool to the user.",
+      },
+      {
+        type: "text",
+        text: "# Design Systems\n\nA design system is a **collection of components** that are used to build a design system. It is a collection of components that are used to build a design system. ",
+      },
+    ],
   },
 ];
 
@@ -24,7 +70,7 @@ export default function ChatMessageDemo() {
   return (
     <div className="flex flex-col gap-4 w-full max-w-lg">
       {messages.map((message) => (
-        <ChatMessage key={message.content} message={message} />
+        <ChatMessage key={message.id} message={message} />
       ))}
     </div>
   );
--- registry/new-york/blocks/chat-message/chat-message.tsx
+++ registry/new-york/blocks/chat-message/chat-message.tsx
@@ -6,12 +6,7 @@ import Markdown from "@/registry/new-york/blocks/markdown/markdown";
 import React from "react";
 import { Button } from "@/registry/new-york/blocks/button/button";
 import { Copy, Check } from "lucide-react";
-
-export interface Message {
-  role: "user" | "assistant";
-  content: string;
-  avatar?: string;
-}
+import { ToolUIPart, UIMessage } from "ai";
 
 const userMessageVariants = cva("flex flex-col gap-2", {
   variants: {
@@ -26,7 +21,7 @@ const assistantMessageVariants = cva("flex flex-col gap-2", {
   variants: {
     variant: {
       message:
-        "bg-gradient-to-b from-muted to-muted/50 rounded-xl px-6 py-4 border shadow-lg shadow-slate-200/50 dark:shadow-none mr-8",
+        "bg-gradient-to-b from-muted to-muted/50 rounded-xl px-4 py-2 border shadow-md shadow-foreground/5 dark:shadow-none mr-8",
       paragraph: "",
     },
   },
@@ -36,13 +31,13 @@ export default function ChatMessage({
   message,
   className,
 }: {
-  message: Message;
+  message: UIMessage;
   className?: string;
 }) {
   const [copied, setCopied] = React.useState(false);
 
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(message.content);
+    navigator.clipboard.writeText("temp");
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
   };
@@ -50,49 +45,29 @@ export default function ChatMessage({
   if (message.role === "user") {
     return (
       <div className={cn(userMessageVariants({ variant: "message" }), className)}>
-        {message.content}
+        {message.parts.map((part) => part.type === "text" && part.text).join("")}
       </div>
     );
   }
 
   return (
     <div className={cn("flex items-start gap-3", className)}>
-      <div className="shrink-0">
-        {message.avatar && (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={message.avatar}
-            alt="Assistant avatar"
-            className="size-8 rounded-full object-cover mt-1"
-          />
-        )}
-      </div>
-
       <div
         className={cn(
           "flex flex-col gap-1 relative",
           assistantMessageVariants({ variant: "message" })
         )}
       >
-        <Markdown>{message.content}</Markdown>
-        <Button
-          onClick={copyToClipboard}
-          variant="ghost"
-          size="iconSm"
-          className="absolute right-1 bottom-1 focus-visible:ring-0 bg-transparent text-muted-foreground hover:bg-muted active:bg-muted"
-          aria-label={copied ? "Copied" : "Copy to clipboard"}
-        >
-          <span className="sr-only">{copied ? "Copied" : "Copy"}</span>
-          <Copy
-            className={cn("size-3 transition-all duration-300", copied ? "scale-0" : "scale-100")}
-          />
-          <Check
-            className={cn(
-              "absolute inset-0 m-auto size-3 transition-all duration-300 text-emerald-500",
-              copied ? "scale-100" : "scale-0"
-            )}
-          />
-        </Button>
+        {message.parts.map((part) => {
+          if (part.type.includes("tool")) {
+            return "Tool";
+          } else if (part.type === "text") {
+            return <Markdown>{part.text}</Markdown>;
+          } else if (part.type === "reasoning") {
+            return <Markdown className="text-sm text-muted-foreground">{part.text}</Markdown>;
+          }
+          return null;
+        })}
       </div>
     </div>
   );
